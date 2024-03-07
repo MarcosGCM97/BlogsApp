e52969926be952f30535e127a79f5ceb4ac86d6c
Marcos Congregado <marcoscongregado@gmail.com>
2024-03-07 15:45:00 -0300

    add build commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 
@@ -111,7 +112,7 @@ app.delete('/api/blogs/:id', (request, response)=>{
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
 })
